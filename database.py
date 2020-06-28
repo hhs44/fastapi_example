5707374605d0242118e6343db5822b0bcaaaea75
huang <1159986871@qq.com>
2020-06-28 16:45:47 +0800

    部署版本

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "mysql://root:mysql@123.56.80.63:3306/carcard?charset=utf8"
+SQLALCHEMY_DATABASE_URL = "mysql://huang:mysql@123.56.80.63:3306/carcard?charset=utf8"
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
